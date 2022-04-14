858c609a6e60113c0109b757362035fab340cc26
45444611899 <45444611899@NOTE0111B1>
2022-04-14 17:12:28 -0300

    adicionado pagina de redirecionamento

--- senaiRHmobile/App.js
+++ senaiRHmobile/App.js
@@ -12,7 +12,7 @@ const AuthStack = createStackNavigator();
 import Login from './src/screens/login';
 import MinhasAtividades from './src/screens/MinhasAtividades';
 import Perfil from './src/screens/perfil';
-import Escolha from './src/screens/escolha';
+import Redirecionamento from './src/screens/redirecionamento';
 import Atividades from './src/screens/atividades';
 import AtividadeComum from './src/screens/atividadeComum';
 // import Teste from './src/screens/teste';
@@ -24,17 +24,16 @@ export default function Stack() {
         hidden={true} />
 
       <AuthStack.Navigator
-        initialRouteName="Login"
+        initialRouteName="Redirecionamento"
         screenOptions={{
           headerShown: false,
         }}>
         <AuthStack.Screen name="Login" component={Login} /> 
         <AuthStack.Screen name="MinhasAtividades" component={MinhasAtividades} />
         <AuthStack.Screen name="Perfil" component={Perfil} />
-        <AuthStack.Screen name="Escolha" component={Escolha} />
+        <AuthStack.Screen name="Redirecionamento" component={Redirecionamento} />
         <AuthStack.Screen name="Atividades" component={Atividades} />
         <AuthStack.Screen name="AtividadeComum" component={AtividadeComum} />
-        {/* <AuthStack.Screen name="Teste" component={Teste} />  */}
       </AuthStack.Navigator>
     </NavigationContainer>
   );
