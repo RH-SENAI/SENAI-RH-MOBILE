3073207f050f7efb43412ba90f886d4c6016f4c4
Vinicius <vini.evangelista@live.com>
2022-05-08 14:37:15 -0300

    feat: Adiciona Gráfico de linhas

--- senaiRH/src/pages/dashboard/Dashboard.js
+++ senaiRH/src/pages/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import react from "react";
 import { View, StyleSheet } from 'react-native';
-import { PieChart } from 'react-native-svg-charts';
+import { PieChart, LineChart, YAxis, Grid } from 'react-native-svg-charts';
 import { Text } from 'react-native-svg'
 
 export default function Dashboard() {
@@ -48,6 +48,11 @@ export default function Dashboard() {
             )
         })
     }
+
+
+    const data3 = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
+    const contentInset = { top: 20, bottom: 20 }
+
     return (
         <View style={styles.container}>
             <PieChart style={styles.pieChart} data={pieData}>
@@ -56,6 +61,26 @@ export default function Dashboard() {
             <PieChart style={styles.pieChart} data={pieData2}>
                 <Label />
             </PieChart>
+            <View style={{ height: 200, flexDirection: 'row' }}>
+                <YAxis
+                    data={data3}
+                    contentInset={contentInset}
+                    svg={{
+                        fill: 'grey',
+                        fontSize: 10,
+                    }}
+                    numberOfTicks={10}
+                    formatLabel={(value) => `${value}ºC`}
+                />
+                <LineChart
+                    style={{ flex: 1, marginLeft: 16 }}
+                    data={data3}
+                    svg={{ stroke: 'rgb(134, 65, 244)' }}
+                    contentInset={contentInset}
+                >
+                    <Grid />
+                </LineChart>
+            </View>
         </View>
     )
 }
