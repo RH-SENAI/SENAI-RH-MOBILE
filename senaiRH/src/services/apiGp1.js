02cb491fc18c8ba14e5bb48f61eff957a6671c65
Samuel De Oliveira Pereira <47429258824@info.scs>
2022-05-26 17:14:39 -0300

    Merge completo

--- senaiRH/src/services/apiGp1.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://apirhsenaigp1.azurewebsites.net/api'
-});
-
-export default api;
--- senaiRH/src/services/apiGp3.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-    baseURL: 'https://apigrupo3.azurewebsites.net/api/'
-})
-
-export default api;
